1cfba5500657a4d02eda56cfb7ee74d2e772e354
Bohan Yang <yang.bohan@optimind.tech>
2025-03-25 16:51:40 +0900

    remove sidebar

--- app/demo/[organizationId]/page.tsx
+++ app/demo/[organizationId]/page.tsx
@@ -26,30 +26,15 @@ export default function Page() {
   const { organizationId } = useParams<{ organizationId: string }>();
   return (
     <QueryClientProvider client={queryClient}>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
-            <div className='flex items-center gap-2 px-4'>
-              <SidebarTrigger className='-ml-1' />
-              <Separator
-                orientation='vertical'
-                className='mr-2 data-[orientation=vertical]:h-4'
-              />
-              {organizationId}
-            </div>
-          </header>
-          <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-            <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-              <ProjectList organizationId={organizationId} />
-              <div className='aspect-video rounded-xl' />
-            </div>
-            <div className='min-h-[100vh] flex-1 rounded-xl md:min-h-min'>
-              <PublishProject organizationId={organizationId} />
-            </div>
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
+      <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
+        <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
+          <ProjectList organizationId={organizationId} />
+          <div className='aspect-video rounded-xl' />
+        </div>
+        <div className='min-h-[100vh] flex-1 rounded-xl md:min-h-min'>
+          <PublishProject organizationId={organizationId} />
+        </div>
+      </div>
     </QueryClientProvider>
   );
 }
